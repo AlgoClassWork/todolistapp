5b29ce48c014e60a79d58674c75095f980ab284e
AlgoClassWork <kyxec.play@gmail.com>
2025-05-21 14:29:04 +0600

    Update database.py

--- database.py
+++ database.py
@@ -9,19 +9,20 @@ def init_database():
     connection = get_connection()
     cursor = connection.cursor()
     cursor.execute('''
-                   CREATE TABLE IF NOT EXISTS tasks (
-                   id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   title TEXT NOT NULL,
-                   description TEXT,
-                   done INTEGER DEFAULT 0)
-                   ''')
+    CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    description TEXT,
+    done INTEGER DEFAULT 0) ''')
     connection.commit()
     connection.close()
 
-def add_task(title, description):
+def add_task(title, description=''):
     connection = get_connection()
     cursor = connection.cursor()
-    cursor.execute('INSERT INTO tasks (title, description) VALUES (?, ?)', (title, description))
+    cursor.execute('''
+    INSERT INTO tasks (title, description)
+    VALUES (?, ?)''', (title, description))
     connection.commit()
     connection.close()
 
