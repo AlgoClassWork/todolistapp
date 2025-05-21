7894e99917c0df9aedf0ca475bb629eb5284c7e1
AlgoClassWork <kyxec.play@gmail.com>
2025-05-21 15:20:25 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template, request, redirect
-from database import *
+import database
 
 app = Flask(__name__)
 
 #http://127.0.0.1:5000/
 @app.route('/')
 def main_page():
-    tasks = get_all_tasks() 
+    tasks = database.get_all_tasks() 
     return render_template('index.html', tasks=tasks)
 
 #http://127.0.0.1:5000/add
 @app.route('/add', methods=['POST'])
 def add_task():
     title = request.form['title']
+    description = request.form['description']
+    if title:
+        database.add_task(title, description)
     return redirect('/')
 
 app.run()
